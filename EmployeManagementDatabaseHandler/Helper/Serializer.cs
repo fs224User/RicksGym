c5945ebb8b1c0fb382e5af204a4a33fbc8ea79e8
fs224User <atiw.fs224.swd@gmail.com>
2024-07-04 10:09:47 +0200

    Delete EmployeManagementDatabaseHandler directory

--- EmployeManagementDatabaseHandler/Helper/Serializer.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EmployeeManagement.Helper;
-
-public static class Serializer
-{
-    private static void DeserializeEmployee()
-    {
-        throw new NotImplementedException();
-    }
-}
